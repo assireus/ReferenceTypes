7f73c79aaf04ae28d6adfdd071ba039e9f53cd9d
Andrew <90501350+assireus@users.noreply.github.com>
2021-11-29 15:04:31 +0300

    Update Program.cs

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,10 +10,18 @@ namespace Task1
             Figure line = new Line(1, 1, 5, 8);
             Figure circle = new Circle(1, 2, 5);
             Figure rectangle = new Rectangle(1, 0, 5, 12);
-            Figure round = new Round(1, 5, 9, "Yellow");
-            Figure ring = new Ring(3, 5, 7, 11);
+            Figure round = new Round(1, 8, 13, "Yellow");
+            Figure ring = new Ring(4, 6, 8, 12);
             List<Figure> figures = new();
             figures.Add(line);
+            figures.Add(circle);
+            figures.Add(rectangle);
+            figures.Add(round);
+            figures.Add(ring);
+            foreach (var figure in figures)
+            {
+                Console.WriteLine(Fogure.Draw())
+            }
         }
     }
 }
